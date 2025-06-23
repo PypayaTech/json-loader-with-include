6f4346974f9c816714ab002f4eaf84c0c1aaf33b
PypayaTech <138917152+PypayaTech@users.noreply.github.com>
2025-06-23 14:36:38 +0200

    Add deprecation warning for package rename to pypaya-json

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="json-loader-with-include",
-    version="0.3.1",
+    version="0.3.3",
     url="https://github.com/PypayaTech/json-loader-with-include",
     author="PypayaTech",
-    description="A simple package that allows you to include (fragments of) external JSON files within a JSON file.",
+    description="⚠️ DEPRECATED: Use 'pypaya-json' instead. Enhanced JSON processing with includes.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     readme="README.md",
